cb971fd57fcea2974837aa2615a785b939f935c0
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2019-04-03 12:20:21 +0200

    ngStyle and ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 
+type Status = 'offline' | 'online';
+
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.scss']
 })
 export class ServerComponent {
-  serverId = 10;
-  serverStatus = 'offline';
+  serverId: number;
+  serverStatus: Status;
 
-  public getServerStatus() {
+  constructor() {
+    this.serverId = Math.ceil(Math.random() * 100);
+    this.serverStatus = Math.random() > 0.5 ? 'offline' : 'online';
+  }
+
+  getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'offline' ? 'darkred' : 'darkgreen';
   }
 }
