0c93302edbd67f459a9a31ae2e8062d837abe90a
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2019-04-04 11:26:32 +0200

    Initialize Recipes app project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-tutorial'`, () => {
+  it(`should have as title 'recipes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-tutorial');
+    expect(app.title).toEqual('recipes');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-tutorial!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to recipes!');
   });
 });
