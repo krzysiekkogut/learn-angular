df8525ee336195916c691f47aacd339698af926d
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2019-04-04 12:19:32 +0200

    Create templates for recipes components

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../models';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
+  public recipes: Recipe[] = [
+    new Recipe(
+      'First recipe',
+      'It\'s only a test',
+      'https://s0.dziennik.pl/pliki/10420000/10420181-tofu-900-554.jpg'
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
