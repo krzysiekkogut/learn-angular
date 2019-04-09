b9346133b6f4f36e16fcf238f954ba1df9a69fb1
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2019-04-09 12:20:38 +0200

    Show ingredients in recipe details

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,19 +1,22 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './models';
+import { Ingredient } from '../shared/models';
 
 export class RecipesService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'First recipe',
-      'It\'s only a test',
-      'https://s0.dziennik.pl/pliki/10420000/10420181-tofu-900-554.jpg'
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel - just awesome!',
+      'https://thestayathomechef.com/wp-content/uploads/2018/03/German-Schnitzel-3-small.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Second recipe',
-      'It\'s only a test',
-      'https://s0.dziennik.pl/pliki/10420000/10420181-tofu-900-554.jpg'
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://www.readersdigest.ca/wp-content/uploads/sites/14/2015/11/gourmet-burger-1024x666.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     )
   ];
 
