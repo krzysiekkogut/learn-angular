739b9f710cb947f3a782f505ab8010c16ec8436f
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2019-04-17 20:22:43 +0200

    Add preload strategy

--- src/app/app.routing-module.ts
+++ src/app/app.routing-module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 
 import { HomeComponent } from './core/home/home.component';
@@ -20,7 +20,9 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
